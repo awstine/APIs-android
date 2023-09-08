80fed2324b1fc7b181a384df689af9acc6ebc84d
Austine Ndeda <awstinejessy@gmail.com>
2023-09-08 06:50:05 -0700

    Updated

--- app/src/main/java/com/example/api_android/data/remote/ThroneResponce.kt
+++ app/src/main/java/com/example/api_android/data/remote/ThroneResponce.kt
@@ -3,79 +3,23 @@ package com.example.api_android.data.remote
 
 import com.google.gson.annotations.SerializedName
 
-data class ThroneResponce(
-    @SerializedName("#comment")
-    val comment: String,
-    @SerializedName("html")
-    val html: Html
-) {
-    data class Html(
-        @SerializedName("body")
-        val body: Body,
-        @SerializedName("head")
-        val head: Head,
-        @SerializedName("@lang")
-        val lang: String
-    ) {
-        data class Body(
-            @SerializedName("app-root")
-            val appRoot: String,
-            @SerializedName("@class")
-            val classX: String,
-            @SerializedName("script")
-            val script: List<Script>,
-            @SerializedName("@style")
-            val style: String
-        ) {
-            data class Script(
-                @SerializedName("@defer")
-                val defer: String,
-                @SerializedName("@src")
-                val src: String
-            )
-        }
-
-        data class Head(
-            @SerializedName("base")
-            val base: Base,
-            @SerializedName("link")
-            val link: List<Link>,
-            @SerializedName("meta")
-            val meta: List<Meta>,
-            @SerializedName("style")
-            val style: List<Style>,
-            @SerializedName("title")
-            val title: String
-        ) {
-            data class Base(
-                @SerializedName("@href")
-                val href: String
-            )
-
-            data class Link(
-                @SerializedName("@href")
-                val href: String,
-                @SerializedName("@rel")
-                val rel: String,
-                @SerializedName("@type")
-                val type: String
-            )
-
-            data class Meta(
-                @SerializedName("@charset")
-                val charset: String,
-                @SerializedName("@content")
-                val content: String,
-                @SerializedName("@name")
-                val name: String
-            )
-
-            data class Style(
-                @SerializedName("#text")
-                val text: String,
-                @SerializedName("@type")
-                val type: String
-            )
-        }
-    }
+class ThroneCharactersResponce : ArrayList<ThroneCharactersResponceItem>(){
+    data class ThroneCharactersResponceItem(
+        @SerializedName("family")
+        val family: String,
+        @SerializedName("firstName")
+        val firstName: String,
+        @SerializedName("fullName")
+        val fullName: String,
+        @SerializedName("id")
+        val id: Int,
+        @SerializedName("image")
+        val image: String,
+        @SerializedName("imageUrl")
+        val imageUrl: String,
+        @SerializedName("lastName")
+        val lastName: String,
+        @SerializedName("title")
+        val title: String
+    )
 }
